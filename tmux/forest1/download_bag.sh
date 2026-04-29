061e292c30cae039e70ff131b5b1bcf0dfb0f5a6
manuelboldrer <manuel.boldrer@gmail.com>
2026-04-29 16:55:13 +0200

    update

--- tmux/forest1/download_bag.sh
+++ tmux/forest1/download_bag.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-URL="https://nasmrs.fel.cvut.cz:3923/PERSONAL_DIRECTORIES/manuel/rosbag2_2026_04_29-11_59_36_0.mcap"
+URL="https://nasmrs.fel.cvut.cz:3923/PERSONAL_DIRECTORIES/manuel/BAGS4GITHUB/rosbag2_2026_04_29-11_59_36_0.mcap"
 OUT_FILE="rosbag.mcap"
 
 if [ -f "$OUT_FILE" ]; then
