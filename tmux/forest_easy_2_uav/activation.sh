da0d59efe11d39e8216ba874434371893649d800
Afzal Ahmad <afzalhmd14@gmail.com>
2026-05-28 16:50:55 +0200

    fixed: tf issues and takeoff issues

--- tmux/forest_easy_2_uav/activation.sh
+++ tmux/forest_easy_2_uav/activation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
+# assign goal
 ros2 service call /$UAV_NAME/rbl_controller/goto mrs_msgs/srv/Vec4 "{goal: [10, -0.0, 2.5, 0.0]}"
-# ros2 service call /uav3/rbl_controller/goto mrs_msgs/srv/Vec4 "{goal: [10, -0.0, 2.5, 0.0]}"
-
+# trigger activation
 ros2 service call /$UAV_NAME/rbl_controller/activation std_srvs/srv/Trigger
-# ros2 service call /uav3/rbl_controller/activation std_srvs/srv/Trigger
---
+++ tmux/forest_easy_2_uav/tf_map_to_sim_frame.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ros2 run tf2_ros static_transform_publisher \
+  --x 0 \
+  --y 0 \
+  --z 0.0 \
+  --roll 0.0 \
+  --pitch 0.0 \
+  --yaw 0.0 \
+  --frame-id simulator_origin --child-frame-id map
