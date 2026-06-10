32470f4aa8b4017cfd2e41d3fe029a2509338dbb
Afzal Ahmad <afzalhmd14@gmail.com>
2026-06-10 16:23:44 +0200

    cleaned: forest easy 2 uavs

--- tmux/forest_easy_2_uav/map_tf_1.sh
+++ tmux/forest_easy_2_uav/map_tf_1.sh
@@ -7,5 +7,4 @@ ros2 run tf2_ros static_transform_publisher \
   --roll 0.0 \
   --pitch 0.0 \
   --yaw 0.0 \
-  --frame-id $UAV_NAME/world_origin\
-  --child-frame-id map
+  --frame-id simulator_origin --child-frame-id $UAV_NAME/world_origin
